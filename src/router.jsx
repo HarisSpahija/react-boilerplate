9d022f5e14c58723f3a4df8edd7040e26b7ae8b3
HarisSpahija <haris.spahija@hybrit.org>
2019-04-19 15:18:14 +0200

    refactor: Change AppRouter to const

--- src/router.jsx
+++ src/router.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FormPage from './components/FormPage';
 import HomePage from './components/HomePage';
 
-function AppRouter() {
+const AppRouter = () => {
   return (
     <Router>
       <Route path="/" exact component={HomePage} />
       <Route path="/form/" component={FormPage} />
     </Router>
   );
-}
+};
 
 export default AppRouter;
