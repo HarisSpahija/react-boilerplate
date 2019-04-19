5688d4a4d564b73a6b66101acbbc607817a7ae27
HarisSpahija <haris.spahija@hybrit.org>
2019-04-19 13:21:12 +0200

    feat: Add yup validation

--- src/components/exampleform/index.jsx
+++ src/components/exampleform/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Formik } from 'formik';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Paper from '@material-ui/core/Paper';
+import * as Yup from 'yup';
 import ExampleForm from './ExampleForm';
 
 const styles = theme => ({
@@ -17,6 +18,19 @@ const styles = theme => ({
   }
 });
 
+const validationSchema = Yup.object({
+  name: Yup.string('Enter a name').required('Name is required'),
+  email: Yup.string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup.string('')
+    .min(8, 'Password must contain at least 8 characters')
+    .required('Enter your password'),
+  confirmPassword: Yup.string('Enter your password')
+    .required('Confirm your password')
+    .oneOf([Yup.ref('password')], 'Password does not match')
+});
+
 class InputForm extends Component {
   constructor(props) {
     super(props);
@@ -25,12 +39,17 @@ class InputForm extends Component {
 
   render() {
     const classes = this.props;
+    const values = { name: '', email: '', confirmPassword: '', password: '' };
     return (
       <React.Fragment>
         <div className={classes.container}>
           <Paper elevation={1} className={classes.paper}>
             <h1>Form</h1>
-            <Formik render={props => <ExampleForm {...props} />} />
+            <Formik
+              render={props => <ExampleForm {...props} />}
+              initialValues={values}
+              validationSchema={validationSchema}
+            />
           </Paper>
         </div>
       </React.Fragment>
