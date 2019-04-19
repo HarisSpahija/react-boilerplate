717081ba6ea717656b1f35760d10ec61b4df9617
HarisSpahija <haris.spahija@hybrit.org>
2019-04-19 15:39:12 +0200

    refactor: Move routing to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
+/* eslint-env browser */
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Pages
 import FormPage from './components/FormPage';
 import HomePage from './components/HomePage';
 
-const AppRouter = () => {
-  return (
-    <Router>
+const routing = (
+  <Router>
+    <div>
       <Route path="/" exact component={HomePage} />
       <Route path="/form/" component={FormPage} />
-    </Router>
-  );
-};
+    </div>
+  </Router>
+);
 
-export default AppRouter;
+ReactDOM.render(routing, document.getElementById('root'));
